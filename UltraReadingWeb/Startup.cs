833c71313f783c17578873df4fdc93809c8f5c7a
Abdelrahman El Kinani <kinani95@outlook.com>
2017-03-08 14:01:26 +0200

    Add project files.

---
+++ UltraReadingWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UltraReadingWeb.Startup))]
+namespace UltraReadingWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
